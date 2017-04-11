4e030ec956068d5425562d516145ced600f13ce9
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-04-10 20:19:10 -0600

    flexbox and vue

--- public/app.js
+++ public/app.js
@@ -1,1 +1,24 @@
 console.log("I MADE IT!!")
+
+var swApp = new Vue({
+	el: "#component",
+	data: {
+	characters: []	
+	}
+})
+
+function fetchData(){
+	fetch('/api/starwars/')
+	.then(blob => blob.json())
+	.then(data =>{
+		swApp.characters = data
+
+	})
+	.catch(e =>e)
+
+
+}
+
+fetchData();
+
+console.log(swApp.data);
